8c30d69c6d789d4704e9ca56915ac6f85ac58967
avi <abhi.uk96@gmail.com>
2020-08-03 22:22:10 +0100

    [update] update loan interface to accomodate date time

--- app/src/Model/Interface/LoanInterface.php
+++ app/src/Model/Interface/LoanInterface.php
@@ -2,7 +2,7 @@
 
 namespace LoanInterface;
 /**
- * create an interface 
+ * create an interface
  * Interface LoanInterface
  * @package LoanInterface]
  */
@@ -11,16 +11,24 @@ interface LoanInterface
 
 
     //loan will have start and end date
-
     public function createLoan():bool;
 
     //loan will have state ( open or close)
     public function loanState():bool;
 
     //loan will have tranches(can be added/removed? )
-    public function addTranche();
+    public function addTranche():bool;
 
     //loan tranche can be removed(?): TO CHECK
-    public function deleteTranche();
+    public function deleteTranche():bool;
+
+    //get tranche for the loan
+    public function getTranches(): array;
+
+    //loan will have start and end date
+    public function getStartDate(): \DateTime;
+
+    //loan will have end date
+    public function getEndDate(): \DateTime;
 
 }
